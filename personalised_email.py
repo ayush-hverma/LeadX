aa95dfe9c676c19aa407a0385ca0474150889cf7
ayush-hverma <150714646+ayush-hverma@users.noreply.github.com>
2025-05-22 15:58:44 +0530

    Update personalised_email.py

--- personalised_email.py
+++ personalised_email.py
@@ -797,7 +797,7 @@ At CoinDCX, we're leading India towards the decentralized future of Web3 with an
     print(type(single_email))
 
     outlook_auth_url = get_outlook_auth_url()
-    st.markdown(f"[**Sign in with Outlook**]({outlook_auth_url})", unsafe_allow_html=True)
+    st.markdown(f'<a href="{outlook_auth_url}" target="_blank"><b>Sign in with Outlook</b></a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
